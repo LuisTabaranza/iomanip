408f9b3f8ecfa5e99e739fa00a480493ff94936c
Kurt Luis Tabaranza <95659315+LuisTabaranza@users.noreply.github.com>
2021-12-17 20:41:48 +0800

    Add files via upload

---
+++ iomanip.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    cout << "MULTIPLICATION TABLE";
+    for (int row = 1; row <= 10; row++)
+    {
+        for (int column = 1; column <= 10; column++)
+        cout << setw(3) << row*column << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
