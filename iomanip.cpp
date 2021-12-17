015c9f60402a55bf0da5efe20547c0f2e616cbb3
Kurt Luis Tabaranza <95659315+LuisTabaranza@users.noreply.github.com>
2021-12-17 20:52:55 +0800

    Update iomanip.cpp

--- iomanip.cpp
+++ iomanip.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "MULTIPLICATION TABLE";
+    cout << "            MULTIPLICATION TABLE";
     for (int row = 1; row <= 10; row++)
     {
         for (int column = 1; column <= 10; column++)
